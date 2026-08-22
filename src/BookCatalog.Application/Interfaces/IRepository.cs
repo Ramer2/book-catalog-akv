e603f368487be5c321bc376c78896e95fad37a7d
Andrii Sysoiev <sisoev2006a@gmail.com>
2026-08-22 16:33:18 +0200

    feat: generic repository interface

---
+++ src/BookCatalog.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookCatalog.Domain.Models;
+
+namespace BookCatalog.Application.Interfaces;
+
+// generic repo interface
+public interface IRepository<T>
+{
+    public Task<List<Book>> GetAllAsync(CancellationToken cancellationToken);
+    public Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    public Task InsertAsync(T entity, CancellationToken cancellationToken);
+    public Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
+    public Task DeleteEntityAsync(T entity, CancellationToken cancellationToken);
+    public Task SaveAsync(CancellationToken cancellationToken);
+}
